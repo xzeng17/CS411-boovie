e12f318e1152223fbac3e59e7387d25a56005e78
Xuankun <xzeng17@illinois.edu>
2021-09-15 18:30:07 -0700

    add endpoint

--- flask/web.py
+++ flask/web.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, SauerkrautFishes!'
+
+@app.route('/team')
+def team():
+    return {
+        'Team_name:' : 'SauerkrautFish',
+        'Team_members' : ['Xuankun Zeng', 'Jidong Huang', 'Yichuan Liu', 'Rodrigues Rohan'],
+        'Project_name' : 'Boovie',
+        'Wiki_URL' : 'https://wiki.illinois.edu/wiki/display/CS411AAFA21/Team+Sauerkraut+Fish+CS+411+Project+1'
+    }
