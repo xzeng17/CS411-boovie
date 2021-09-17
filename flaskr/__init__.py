fce3c7427ae0797f79c8f8c49003eae09c018929
Xuankun <xzeng17@illinois.edu>
2021-09-17 00:48:06 -0700

    environment var + local db connection

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,8 +1,24 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS, cross_origin
+from dotenv import load_dotenv
+import os
+from flaskext.mysql import MySQL
+from . import db
+
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
+
+load_dotenv()
+
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = os.getenv('MYSQL_LOCAL_DB_USER')
+app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('MYSQL_LOCAL_ROOT_PASSWORD')
+app.config['MYSQL_DATABASE_DB'] = os.getenv('MYSQL_LOCAL_DB_NAME')
+app.config['MYSQL_DATABASE_HOST'] = os.getenv('MYSQL_LOCAL_DB_HOST')
+mysql.init_app(app)
+conn = mysql.connect()
+
 
 @app.route('/')
 def hello():
@@ -16,3 +32,8 @@ def team():
         'Project_name' : 'Boovie',
         'Wiki_URL' : 'https://wiki.illinois.edu/wiki/display/CS411AAFA21/Team+Sauerkraut+Fish+CS+411+Project+1'
     }
+
+@app.route('/showtablecontent', methods=['GET'])
+def showtablecontent():
+    table_name = request.args.get('table')
+    return db.read_from_table(conn, table_name)
